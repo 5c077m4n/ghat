f0d4f35d3da0c5dc603a2e8b222d580f096ae169
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2023-09-02 00:07:28 +0300

    Add chat init and contact selection

--- src/main.zsh
+++ src/main.zsh
@@ -1,1 +1,14 @@
-#!/usr/bin/env zsh
+() {
+	local app_dir="${1:-.}"
+	cd $app_dir || exit 1
+
+	if [[ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" != "true" ]]; then
+		git init
+		git branch --move me
+	fi
+
+	while true; do
+		local contact="$(gum choose --limit 1 $(git branch --format='%(refname:short)'))"
+		git checkout $contact
+	done
+} "$@"
