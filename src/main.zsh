bae3764b4727f0dfa23ba0f8188ceb5a2044c8c4
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2023-09-02 00:07:28 +0300

    Add action selection

--- src/main.zsh
+++ src/main.zsh
@@ -5,10 +5,33 @@
 	if [[ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" != "true" ]]; then
 		git init
 		git branch --move me
+		git commit --allow-empty --message 'Start talking here...'
 	fi
 
 	while true; do
-		local contact="$(gum choose --limit 1 $(git branch --format='%(refname:short)'))"
-		git checkout $contact
+		local action="$(echo 'PWD\nWrite\nMessages\nContacts\nPull\nEXIT' | gum filter)"
+		case "$action" in
+			PWD)
+				gum confirm "$(pwd)"
+				;;
+			Write)
+				git commit --allow-empty --message "$(gum write --placeholder "What do you want to say? (CTRL+D to finish)")"
+				;;
+			Messages)
+				git log --format='%H' | fzf --preview 'git show {}'
+				;;
+			Contacts)
+				local contact="$(gum choose --limit 1 $(git branch --format='%(refname:short)'))"
+				git checkout $contact
+				;;
+			Pull)
+				gum spin --title "Pulling..." -- git pull --rebase "$(git branch --show-current)"
+				;;
+			EXIT)
+				exit
+				;;
+			*)
+				;;
+		esac
 	done
 } "$@"
