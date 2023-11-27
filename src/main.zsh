310e970dc63a3b076fe3a8e5781136b1a22c1e0e
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2023-11-27 20:19:33 +0200

    Lint

--- src/main.zsh
+++ src/main.zsh
@@ -15,34 +15,34 @@
 		case "$action" in
 			Write)
 				local commit_msg="$(gum write --placeholder "What do you want to say? (CTRL+D to finish)")"
-				git commit --allow-empty --message "$commit_msg"
-				;;
-			Messages)
-				git log --format='%h' | fzf --preview 'git show {}'
-				;;
-			Reword)
-				local msg_hash="$(git log --format='%H' | fzf --preview 'git show {}')"
+					git commit --allow-empty --message "$commit_msg"
+					;;
+				Messages)
+					git log --format='%h' | fzf --preview 'git show {}'
+					;;
+				Reword)
+					local msg_hash="$(git log --format='%H' | fzf --preview 'git show {}')"
 
-				git commit --fixup="reword:$msg_hash" --allow-empty
-				git rebase --autostash --autosquash --interactive "${msg_hash}^"
-				;;
-			Contacts)
-				local contact="$(git branch --format='%(refname:short)' | gum filter)"
-				git checkout "$contact"
-				;;
-			NewContact)
-				local contact="$(gum input --placeholder "New contact's name")"
-				git checkout -b "$contact"
-				;;
-			Pull)
-				gum spin --title "Pulling..." -- git pull --rebase origin "$(git branch --show-current)"
-				;;
-			EXIT)
-				exit 0
-				;;
-			*)
-				exit 1
-				;;
-		esac
-	done
-} "$@"
+					git commit --fixup="reword:$msg_hash" --allow-empty
+					git rebase --autostash --autosquash --interactive "${msg_hash}^"
+					;;
+				Contacts)
+					local contact="$(git branch --format='%(refname:short)' | gum filter)"
+					git checkout "$contact"
+					;;
+				NewContact)
+					local contact="$(gum input --placeholder "New contact's name")"
+					git checkout -b "$contact"
+					;;
+				Pull)
+					gum spin --title "Pulling..." -- git pull --rebase origin "$(git branch --show-current)"
+					;;
+				EXIT)
+					exit 0
+					;;
+				*)
+					exit 1
+					;;
+			esac
+		done
+	} "$@"
